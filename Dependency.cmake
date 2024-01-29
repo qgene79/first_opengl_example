6692f5e1c0c86a17abeac61158b75c6f219cca46
qgene79 <103479839+qgene79@users.noreply.github.com>
2024-01-29 17:25:12 +0900

    glad 추가, glfwSetFramebufferSizeCallback 사용 테스트

--- Dependency.cmake
+++ Dependency.cmake
@@ -32,7 +32,7 @@ set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>)
 ExternalProject_Add(
     dep_glfw
     GIT_REPOSITORY "https://github.com/glfw/glfw.git"
-    GIT_TAG "master"
+    GIT_TAG "3.3.2"
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
     PATCH_COMMAND ""
@@ -48,3 +48,19 @@ ExternalProject_Add(
     )
 set(DEP_LIST ${DEP_LIST} dep_glfw)
 set(DEP_LIBS ${DEP_LIBS} glfw3)
+
+# glad
+ExternalProject_Add(
+    dep_glad
+    GIT_REPOSITORY "https://github.com/Dav1dde/glad"
+    GIT_TAG "v0.1.34"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+        -DGLAD_INSTALL=ON
+    TEST_COMMAND ""
+    )
+set(DEP_LIST ${DEP_LIST} dep_glad)
+set(DEP_LIBS ${DEP_LIBS} glad)
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <spdlog/spdlog.h>
+#include <glad/glad.h> //glad를 glfw 보다 먼저 인쿨루드
 #include <GLFW/glfw3.h>
+
+void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
+    SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
+    glViewport(0, 0, width, height);
+}
 
 int main(int argc, const char** argv) {
     SPDLOG_INFO("Start program");
@@ -12,6 +18,10 @@ int main(int argc, const char** argv) {
         return -1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     // glfw 윈도우 생성, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Create glfw window");
     auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
@@ -21,6 +31,21 @@ int main(int argc, const char** argv) {
         glfwTerminate();
         return -1;
     }
+    //Opengl context 추가
+    glfwMakeContextCurrent(window);
+
+    // glad를 활용한 OpenGL 함수 로딩(Opengl context 생성 이후 실행)
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        SPDLOG_ERROR("failed to initialize glad");
+        glfwTerminate();
+        return -1;
+    }
+    // 이 시점 부터 opengl 사용가능
+    auto glVersion = glGetString(GL_VERSION);
+    //SPDLOG_INFO("OpenGL context version: {}", glVersion); core.h static_assert failed: 'Formatting of non-void pointers is disallowed.'
+    printf("OpenGL context version: %s\n", glVersion);
+
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
 
     // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
