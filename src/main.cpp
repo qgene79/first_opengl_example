d0027ee5ddb04196b930c57a1641c47ac021008c
qgene79 <103479839+qgene79@users.noreply.github.com>
2024-01-29 15:09:35 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
+
+int main(int argc, const char** argv) {
+    SPDLOG_INFO("Start program");
+
+    SPDLOG_INFO("Initialize glfw");
+    if (!glfwInit()) {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_INFO("failed to initalize glfw: {}", description);
+        return -1;
+    }
+
+    // glfw 윈도우 생성, 실패하면 에러 출력후 종료
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
+      nullptr, nullptr);
+    if (!window) {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
